dcc7430bc5d09a796ac85cb74364ba3217994242
Álvaro Entrena Casas <aentrena@MacBook-Air-de-Alvaro.local>
2025-07-23 12:26:55 +0200

    Dia 2 con layout lineal y constraints

---
+++ Dia_2/DragonBall19/app/src/main/java/com/keepcoding/dragonball19/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.keepcoding.dragonball19
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.layout_prueba1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        println("El log más básico")
+        Log.v("MainActivity", "Soy log.v")
+        Log.d("MainActivity", "Soy log.d")
+        Log.w("MainActivity", "Soy log.w")
+        Log.e("MainActivity", "Soy log.e")
+
+    }
+}
